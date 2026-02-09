adbc28a1366838d3bc3739025b0f6e6c8740c844
med-000 <med.rk000@gmail.com>
2026-02-09 20:30:40 +0900

    chore: initial setup completed

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render #後から使うよ
-from django.http import HttpResponse #djangoから簡易的なテキストを表示する関数を引っ張ってくる
+from django.shortcuts import render 
+from django.http import HttpResponse 
 
-# Create your views here.
-def index(request): #indexが呼び出されたら、、、
-    return HttpResponse('<h1>Hey, Welcome</h1>')#HttpResponse('<h1>Hey, Welcome</h1>')をreturnする
+def index(request): 
+    return render(request,'index.html')
